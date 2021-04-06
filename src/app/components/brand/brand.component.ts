65573c012d65e41487e678cf31efa3cea604628f
Metin Onur <metinonur09@gmail.com>
2021-04-07 00:01:29 +0300

    ,

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Brand } from 'src/app/models/brand';
-import{HttpClient} from '@angular/common/http'
-import { BrandResponseModel } from 'src/app/models/brandResponseModel';
 import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brand',
-  templateUrl: './brand.component.html',
+  templateUrl:'./brand.component.html',
   styleUrls: ['./brand.component.css']
 })
 export class BrandComponent implements OnInit {
   brands:Brand[]=[];
+  currentBrand:Brand;
   constructor(private brandService:BrandService) { }
 
   ngOnInit(): void {
@@ -24,4 +23,15 @@ export class BrandComponent implements OnInit {
     })
   }
 
+  setCurrentBrand(brand:Brand){
+    this.currentBrand=brand;
+  }
+  getCurrentBrandClass(brand:Brand){
+    if(brand==this.currentBrand){
+      return "list-group item active"
+    }
+    else{
+      return "list-group item"
+    }
+  }
 }
