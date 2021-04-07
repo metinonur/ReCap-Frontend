5db23d6dcc171514adb80cedef29786c8bedbf43
Metin Onur <metinonur09@gmail.com>
2021-04-07 23:11:25 +0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { RentalComponent } from './components/rental/rental.component';
 const routes: Routes = [
   {path:"",pathMatch:"full",component:CarComponent},
   {path:"cars",component:CarComponent},
+  {path:"cars/brand/:brandId",component:CarComponent},
+  {path:"cars/color/:colorId",component:CarComponent},
   {path:"rentals",component:RentalComponent}
 ];
 
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -8,11 +8,20 @@ import { ListResponseModel } from '../models/listResponseModel';
   providedIn: 'root'
 })
 export class CarService {
-  apiUrl ="https://localhost:44367/api/cars/getall"
+  apiUrl ="https://localhost:44367/api/"
   constructor(private httpClient:HttpClient) { }
   
   getCars():Observable<ListResponseModel<Car>>{
-    return this.httpClient.get<ListResponseModel<Car>>(this.apiUrl)
+    let newPath= this.apiUrl+"cars/getcardetail"
+    return this.httpClient.get<ListResponseModel<Car>>(newPath)
   }
-
+  getCarsByColor(colorId:number):Observable<ListResponseModel<Car>>{
+    let newPath= this.apiUrl+"cars/getbycolor?colorId="+colorId
+    return this.httpClient.get<ListResponseModel<Car>>(newPath)
+  }
+  getCarsByBrand(brandId:number):Observable<ListResponseModel<Car>>{
+    let newPath= this.apiUrl+"cars/getbybrand?brandId="+brandId
+    return this.httpClient.get<ListResponseModel<Car>>(newPath)
+  }
 }
+
